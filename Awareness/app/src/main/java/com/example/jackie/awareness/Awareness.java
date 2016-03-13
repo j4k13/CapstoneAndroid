0e2278ef5cb7e4360c56c249e4a0563bc8ccaf04
j4k13 <namlesscrusader@gmail.com>
2016-03-12 19:17:02 -0500

    Initial bits

---
+++ Awareness/app/src/main/java/com/example/jackie/awareness/Awareness.java
@@ -0,0 +1,23 @@
+package com.example.jackie.awareness;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class Awareness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_awareness);
+        WebView myAware = (WebView) findViewById(R.id.webview);
+        myAware.loadUrl("");
+    }
+
+}
